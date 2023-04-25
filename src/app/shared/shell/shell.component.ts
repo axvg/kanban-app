c7d685a276b5fee6865f2b759ea7b77eb1068d34
axvg <91773368+axvg@users.noreply.github.com>
2023-04-24 22:20:23 -0500

    Update shell component

--- src/app/shared/shell/shell.component.ts
+++ src/app/shared/shell/shell.component.ts
@@ -1,10 +1,18 @@
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Component } from '@angular/core';
+import { Observable, map, shareReplay } from 'rxjs';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
-  styleUrls: ['./shell.component.scss']
+  styleUrls: ['./shell.component.scss'],
 })
 export class ShellComponent {
-
+  isHandset$: Observable<boolean> = this.breakPointObserver
+    .observe([Breakpoints.Handset])
+    .pipe(
+      map((result) => result.matches),
+      shareReplay()
+    );
+  constructor(private breakPointObserver: BreakpointObserver) {}
 }
