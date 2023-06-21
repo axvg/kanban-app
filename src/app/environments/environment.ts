9f51758f808688f07bd0f0e4a7a5d64c046bc4c0
axvg <91773368+axvg@users.noreply.github.com>
2023-06-21 00:48:28 -0500

    update firebase

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,11 @@
 export const environment = {
   production: false,
-  supabaseUrl: '${process.env.SUPABASE_URL}',
-  supabaseKey: '${process.env.SUPABASE_API_KEY}',
+  firebase:  {
+    apiKey: "AIzaSyA4R_7u76gvsTN2KvYFtWc0F1E4oh5RPwU",
+    authDomain: "kanban-app-b4319.firebaseapp.com",
+    projectId: "kanban-app-b4319",
+    storageBucket: "kanban-app-b4319.appspot.com",
+    messagingSenderId: "673100906278",
+    appId: "1:673100906278:web:5ebf436d39a453a9e14be7"
+  }
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebase: {
+    projectId: 'kanban-app-b4319',
+    appId: '1:673100906278:web:5ebf436d39a453a9e14be7',
+    storageBucket: 'kanban-app-b4319.appspot.com',
+    apiKey: 'AIzaSyA4R_7u76gvsTN2KvYFtWc0F1E4oh5RPwU',
+    authDomain: 'kanban-app-b4319.firebaseapp.com',
+    messagingSenderId: '673100906278',
+  },
+};
